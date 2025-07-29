bb41a89141a415b43ce7402f2ce2ae3b031b116f
Pedro Vinicius Wrublak <wrublakpedro@gmail.com>
2025-07-29 07:22:23 -0300

    Working on responsive menu

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -8,7 +8,6 @@ const luckiestGuy = Luckiest_Guy({
 });
 
 type NavBarProps = {
-
   isSidebarOpen: boolean;
   toggleSidebar: () => void;
 };
@@ -18,26 +17,27 @@ function Navbar({ isSidebarOpen, toggleSidebar }: NavBarProps) {
     <header className="bg-amber-50 h-50">
       {/* Menu */}
       <nav>
-        <div
-          className="fixed top-20 left-30 flex items-center gap-2 z-50 hover:opacity-40 text-black"
-          onClick={toggleSidebar}
-        >
+        <div className="fixed top-20 left-30 z-50" onClick={toggleSidebar}>
           {isSidebarOpen ? (
-            <>
-              <X className={`${luckiestGuy.className} w-6 h-6 text-2xl hover:opacity-40 text-gray-400 select-none cursor-pointer`} />
+            <div className="flex items-center gap-2 text-gray-500 hover:text-red-400">
+              <X
+                className={`${luckiestGuy.className} w-6 h-6 text-2xl  select-none cursor-pointer`}
+              />
               <span
-                className={`${luckiestGuy.className} text-2xl hover:opacity-40 text-gray-400 select-none cursor-pointer`}
-              >Close</span>
-            </>
+                className={`${luckiestGuy.className} text-2xl  select-none cursor-pointer`}
+              >
+                Close
+              </span>
+            </div>
           ) : (
-            <>
+            <div className="flex items-center gap-2 text-black hover:opacity-50">
               <Menu className="w-6 h-6" />
               <span
                 className={`font-medium text-2xl select-none uppercase cursor-pointer ${luckiestGuy.className}`}
               >
                 Menu
               </span>
-            </>
+            </div>
           )}
         </div>
       </nav>
--- src/app/components/SideBar/index.tsx
+++ src/app/components/SideBar/index.tsx
@@ -1,28 +1,40 @@
 import { X } from "lucide-react";
-import { Luckiest_Guy } from 'next/font/google';
+import { Luckiest_Guy } from "next/font/google";
 
 const luckiestGuy = Luckiest_Guy({
-    subsets: ['latin'],
-    weight: '400',
-  });
-
+  subsets: ["latin"],
+  weight: "400",
+});
 
 type SideBarProps = {
-    isOpen: boolean;
-    onClose: () => void;
-}
+  isOpen: boolean;
+  onClose: () => void;
+};
 
 function SideBar({ isOpen, onClose }: SideBarProps) {
-    return (
-        <div
-        className={`fixed top-0 left-0 h-full w-[30%] bg-white  shadow-lg transform transition-transform duration-300 z-40 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
-            <ul className={`mt-50 px-20 space-y-10 flex flex-col justify-center ${luckiestGuy.className} text-black select-none`}>
-                <li className="text-5xl hover:text-blue-400 cursor-pointer">Home</li>
-                <li className="text-5xl hover:text-blue-400 cursor-pointer">Projects</li>
-                <li className="text-5xl hover:text-blue-400 cursor-pointer">Contact</li>
-            </ul>
-        </div>
-    );
+  return (
+    <div
+      className={`fixed  z-40  bg-white  shadow-lg transition-transform duration-300  ${luckiestGuy.className} 
+        
+        md:top-0 md:left-0 md:h-full md:w-[30%] translate-x-0
+        ${isOpen ? "translate-x-0" : "md:-translate-x-full"}
+        
+        top-0 left-0 w-full h-[50%]
+        ${isOpen ? "translate-y-0" : "-translate-y-full"}
+
+        md:translate-y-0
+        
+        `}
+    >
+      <ul
+        className={`mt-70 px-30 space-y-10 flex flex-col justify-cente text-3xl md:text-5xl text-black select-none`}
+      >
+        <li onClick={onClose} className="hover:text-blue-400 cursor-pointer">Home</li>
+        <li onClick={onClose} className="hover:text-blue-400 cursor-pointer">Projects</li>
+        <li onClick={onClose} className="hover:text-blue-400 cursor-pointer">Contact</li>
+      </ul>
+    </div>
+  );
 }
 
 export default SideBar;
