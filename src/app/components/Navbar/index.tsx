db23d6f48c153f61f7fb9a5d10e21d37cf90bd01
Pedro Vinicius Wrublak <wrublakpedro@gmail.com>
2025-07-30 07:20:52 -0300

    Added 'hire me' button

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -14,18 +14,17 @@ type NavBarProps = {
 
 function Navbar({ isSidebarOpen, toggleSidebar }: NavBarProps) {
   return (
-    <header className="bg-amber-50 h-50">
+    <header className="bg-amber-50 h-[10rem] py-[4rem] px-[7rem]">
       {/* Menu */}
-      <nav>
-        <div className="fixed top-20 left-30 z-50" onClick={toggleSidebar}>
+      <nav className="flex justify-between items-center">
+        <div
+          className="z-50 cursor-pointer select-none"
+          onClick={toggleSidebar}
+        >
           {isSidebarOpen ? (
-            <div className="flex items-center gap-2 text-gray-500 hover:text-red-400">
-              <X
-                className={`${luckiestGuy.className} w-6 h-6 text-2xl  select-none cursor-pointer`}
-              />
-              <span
-                className={`${luckiestGuy.className} text-2xl  select-none cursor-pointer`}
-              >
+            <div className="flex items-center gap-2 text-black hover:opacity-50">
+              <X className={`${luckiestGuy.className} w-6 h-6 text-2xl `} />
+              <span className={`${luckiestGuy.className} uppercase text-2xl`}>
                 Close
               </span>
             </div>
@@ -33,12 +32,22 @@ function Navbar({ isSidebarOpen, toggleSidebar }: NavBarProps) {
             <div className="flex items-center gap-2 text-black hover:opacity-50">
               <Menu className="w-6 h-6" />
               <span
-                className={`font-medium text-2xl select-none uppercase cursor-pointer ${luckiestGuy.className}`}
+                className={`font-medium text-2xl uppercase ${luckiestGuy.className}`}
               >
                 Menu
               </span>
             </div>
           )}
+        </div>
+
+        {/* Hire me button */}
+        <div className="z-50 cursor-pointer select-none">
+          <a
+            href="#contact"
+            className={`text-blue-300 inline-block hover:scale-x-150 transition-transform ease-in-out ${luckiestGuy.className} text-2xl font-bold uppercase`}
+          >
+            Hire me
+          </a>
         </div>
       </nav>
     </header>
